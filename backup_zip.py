2253f24b2f7dd06498f207bdd06fa460ad320b77
DawnCom1ng <xiangruli1231@gmail.com>
2018-10-22 20:22:41 +0800

    backup by zip

---
+++ backup_zip.py
@@ -0,0 +1,32 @@
+import os
+import time
+
+source = ['"D:\\world"']
+target_dir = 'D:\\Backup'
+
+if not os.path.exists(target_dir):
+    os.mkdir(target_dir)
+
+today = target_dir + os.sep + time.strftime(%Y%m%d)
+
+now = time.strftime(%H%M%S)
+
+comments = input("Enter a comment --->")
+if len(comments) == 0:
+    target = today + os.sep + now + '.zip'
+else:
+    target = today + os.sep + now + '_' + comment.replace(' ', '_') + '.zip'
+
+if os.path.exists(today):
+    os.mkdir(today)
+
+zip_command = 'zip -r {0} {1}'.format(target, ' '.join(source))
+
+print('Zip command is:')
+print(zip_command)
+print('Running')
+if os.system(zip_command) == 0:
+    print('Successful backup to', target)
+else:
+    print('Backup Failed')
+
